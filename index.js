a1a065249ea960fb8becaf76c53efcdf618a31d3
TMATTHEWS23 <136655813+TMATTHEWS23@users.noreply.github.com>
2023-06-21 19:40:59 -0500

    John assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+// VARIABLES
+// LET, var, const
+
+const name = "pizza";
+
+// console.log(1); // Print to the console
+const topping = "popcorn";
+console.log(name, topping);
+
+// TEMPLATE LITERALS
+console.log(`${name} ${topping}`);
+
+console.log("string" + "1");
+
+// integer
+
+// booleans
+
+// console.log(Boolean(name));
+// let prompt1 = prompt("how are you ?");
+// let whereDidHeGo = prompt("where did he go");
+
+// console.log(`${prompt1} went to the ${whereDidHeGo}`);
+
+// alert(`${prompt1} went to the ${whereDidHeGo}`);
+
+let prompt2 = prompt("What is your name ");
+let prompt3 = prompt("How old are you?");
+let prompt4 = prompt("what is your hobby");
+alert(
+  `Hi my name is ${prompt2}, I am ${prompt3}, and I like playing ${prompt4}.`
+);
